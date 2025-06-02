8e2ab9794d4c701c28d6df2617a0c7c531bdc7f1
juanmaherrera94 <101391003+juanmaherrera94@users.noreply.github.com>
2025-06-02 18:59:48 +0200

    mas pruebas

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,47 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors"); 
-const pool = require("./db");
+  require("dotenv").config();
+  const express = require("express");
+  const cors = require("cors"); 
+  const pool = require("./db");
 
-const app = express();
-const port = process.env.PORT || 4000
+  const app = express();
+  const port = process.env.PORT || 4000
 
-// Configuración de CORS para permitir solicitudes desde localhost:5173 (tu frontend Vue)
-const corsOptions = {
-  origin: "http://localhost:5173",  // Permite solicitudes desde el frontend
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"] // Permite el encabezado Content-Type
-};
+  // Configuración de CORS para permitir solicitudes desde localhost:5173 (tu frontend Vue)
+  const corsOptions = {
+    origin: "http://localhost:5173",  // Permite solicitudes desde el frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"] // Permite el encabezado Content-Type
+  };
 
-// Usar CORS con las opciones especificadas
-app.use(cors(corsOptions));
+  // Usar CORS con las opciones especificadas
+  app.use(cors());
 
-// Middleware para poder leer los datos en formato JSON de las solicitudes
-app.use(express.json());
+  // Middleware para poder leer los datos en formato JSON de las solicitudes
+  app.use(express.json());
 
-// Rutas
-const usuariosRoutes = require("./routes/usuarios");
-app.use("/api/usuarios", usuariosRoutes);
+  // Rutas
+  const usuariosRoutes = require("./routes/usuarios");
+  app.use("/api/usuarios", usuariosRoutes);
 
-const emparejamientosRoutes = require("./routes/emparejamientos");
-app.use("/api/emparejamientos", emparejamientosRoutes);
+  const emparejamientosRoutes = require("./routes/emparejamientos");
+  app.use("/api/emparejamientos", emparejamientosRoutes);
 
-const reservasRoutes = require('./routes/reservas')
-app.use('/api/reservas', reservasRoutes)
-
-
-const jugadoresRoutes = require("./routes/jugadores");
-app.use("/api/jugadores", jugadoresRoutes);
+  const reservasRoutes = require('./routes/reservas')
+  app.use('/api/reservas', reservasRoutes)
 
 
-app.get("/", (req, res) => {
-  res.send("Servidor funcionando correctamente");
-});
+  const jugadoresRoutes = require("./routes/jugadores");
+  app.use("/api/jugadores", jugadoresRoutes);
 
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`🚀 Servidor corriendo en el puerto ${port}`);
-});
+  app.get("/", (req, res) => {
+    res.send("Servidor funcionando correctamente");
+  });
+
+
+  // Iniciar el servidor
+  app.listen(port, () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${port}`);
+  });
 
 
