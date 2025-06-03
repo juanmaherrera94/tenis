f63e902847a078ff2a882e0224e4a7f2f0c319a1
juanmaherrera94 <101391003+juanmaherrera94@users.noreply.github.com>
2025-06-03 08:38:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000;
 
 // CORS para desarrollo (en producción ya no necesitas esto si todo se sirve desde el mismo dominio)
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://tenis-production.up.railway.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
 };
